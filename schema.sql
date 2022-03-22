3a3304ca1bfe25f89abffb04789f27cf4a194e43
mar <marilirulita@gmail.com>
2022-03-21 19:27:42 -0700

    Add create table schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id integer,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal
 );
