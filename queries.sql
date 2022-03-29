4707782093ee1568d8d9dd5efef7097a64b25c76
mar <marilirulita@gmail.com>
2022-03-29 12:31:49 -0700

    Add queries explain analize

--- queries.sql
+++ queries.sql
@@ -64,3 +64,8 @@ WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name
 ORDER BY visits DESC
 LIMIT 1;
+
+/* Database performance audit */
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
